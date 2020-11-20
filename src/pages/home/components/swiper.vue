422844e65d9416991e1a37daf4ae9b834c9aab1d
wenjun <1160935483@qq.com>
2020-11-20 22:25:05 +0800

    swiper

---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="wrap">
+     <swiper :options="swiperOptions" >
+       <swiper-slide v-for="item of swiperList" :key="item.id">
+           <img class="swiper-image" alt="" :src="item.imgUrl">
+        </swiper-slide>
+       <div class="swiper-pagination" slot="pagination"></div>
+   </swiper>
+</div>
+</template>
+<script>
+export default {
+    name:"HomeSwiper",
+    data(){
+       return {
+             swiperOptions:{
+                 pagination:'.swiper-pagination',
+                 loop:true
+             },  
+             swiperList:[
+                 {
+                     id:'0001',
+                     imgUrl:"//imgs.qunarzz.com/vs_ceph_vcimg/569cae4ae98e9793f7341e85eed73c54.jpeg"
+                 },{
+                     id:"0002",
+                     imgUrl:"//imgs.qunarzz.com/vs_ceph_vcimg/1cfbdaa2682ada2f854ea6114b3677c8.jpeg"
+                 }
+             ]     
+       } 
+    }
+}
+</script>
+<style lang="stylus" scoped>
+.wrap>>>.swiper-pagination-bullet-active
+ background:#fff!important
+.wrap
+  width:100%
+  height:0
+  padding-bottom:31.25%
+  overflow:hidden
+  .swiper-image
+    width:100%  
+ 
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,6 +2,7 @@
  <div>
     <div>
         <home-header></home-header>
+        <home-swiper></home-swiper>
     </div>
   
  </div>
@@ -9,10 +10,12 @@
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name:"Home",
   components:{
-      HomeHeader
+      HomeHeader,
+      HomeSwiper
   }
 }
 </script>
