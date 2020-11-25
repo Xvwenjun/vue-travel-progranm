14a698f06985f8046b1480bb6c3ff2ed1a51fcdd
wenjun <1160935483@qq.com>
2020-11-25 16:51:27 +0800

    city-list css finished, add alphabet

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,16 +2,23 @@
 <div>
     <home-city></home-city>
     <search-city></search-city>
+    <list-city></list-city>
+    <city-alphabet></city-alphabet>
 </div>
 </template>
 <script>
 import HomeCity from './components/header'
 import SearchCity from './components/search'
+import ListCity from './components/list'
+import CityAlphabet from './components/alphabet'
 export default {
     name:"City",
     components:{
         HomeCity,
-        SearchCity
+        SearchCity,
+        ListCity,
+        CityAlphabet
+      
     }
 }
 </script>
